e31681059d3d5ce34c47fba948c0e67de419159c
answer0424 <wonwon123123@naver.com>
2025-01-29 15:00:07 +0900

    Feat(#8): Fitple.jsx import 수정

--- fitple/src/Fitple.jsx
+++ fitple/src/Fitple.jsx
@@ -1,5 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+import LoginPage from './mainpage/pages/LoginPage';
+import UserRegister from './mainpage/pages/UserRegister';
+import TrainerRegister from './mainpage/pages/TrainerRegister';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginContextProvider from './mainpage/contexts/LoginContextProvider';
+import MainPage from './mainpage/pages/MainPage';
 
 function Fitple() {
   return (
