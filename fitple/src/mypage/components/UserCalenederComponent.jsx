52e8e9ea17b736c579a36a8d08d4227188b815e8
Xuswnss <cooconut0226@gmail.com>
2025-01-31 23:25:50 +0900

    Feat(#14) : component. page 추가

--- fitple/src/mypage/components/UserCalenederComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserCalenederComponent = () => {
-  return <div></div>;
-};
-
-export default UserCalenederComponent;
--- fitple/src/mypage/pages/StudentMyPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StudentMyPage = () => {
-  return <div></div>;
-};
-
-export default StudentMyPage;
