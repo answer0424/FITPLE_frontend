6b4c9ebbccc44b00d413e7cd48bf328fa6eedb09
Xuswnss <cooconut0226@gmail.com>
2025-02-03 12:08:55 +0900

    충돌 수정

--- fitple/src/Fitple.jsx
+++ fitple/src/Fitple.jsx
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-import { useState } from 'react'
+
+
 import './App.css'
 import LoginPage from './mainpage/pages/LoginPage';
 import UserRegister from './mainpage/pages/UserRegister';
@@ -14,37 +13,9 @@ import QuizResult from './quizpage/pages/QuizResult';
 import HBTIListpage from "./HBTIpage/pages/HBTIListpage";
 import HBTIListDetailPage from "./HBTIpage/pages/HBTIListDetailPage";
 import TrainerDetailPage from "./trainerpage/pages/TrainerDetailPage";
-
-
-=======
-import { useState } from "react";
-import "./App.css";
-import LoginPage from "./mainpage/pages/LoginPage";
-import UserRegister from "./mainpage/pages/UserRegister";
-import TrainerRegister from "./mainpage/pages/TrainerRegister";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginContextProvider from "./mainpage/contexts/LoginContextProvider";
-import MainPage from "./mainpage/pages/MainPage";
 import ResetPasswordPage from "./mainpage/pages/ResetPasswordPage";
 import ForgotPasswordPage from "./mainpage/pages/ForgoatPasswordPage";
->>>>>>> ResetPassword
-
-
-
-
-
-
-=======
-import { useState } from "react";
-import "./App.css";
-import LoginPage from "./mainpage/pages/LoginPage";
-import UserRegister from "./mainpage/pages/UserRegister";
-import TrainerRegister from "./mainpage/pages/TrainerRegister";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginContextProvider from "./mainpage/contexts/LoginContextProvider";
-import MainPage from "./mainpage/pages/MainPage";
-import TrainerDetailPage from "./mypage/pages/TrainerDetailPage";
->>>>>>> TrainerDetailPage
+import TrainerDetailPageWrite from './mypage/pages/TrainerDetailPage';
 
 function App() {
   return (
@@ -57,47 +28,28 @@ function App() {
             <Route path="/login" Component={LoginPage}></Route>
             <Route path="/register/user" Component={UserRegister}></Route>
             <Route path="/register/trainer" Component={TrainerRegister}></Route>
-<<<<<<< HEAD
-<<<<<<< HEAD
             <Route path="/quiz" Component={QuizPage}></Route>
             <Route path="/quiz/:userId/result" Component={QuizResult}> </Route>
             <Route path="/hbti" element={<HBTIListpage />} />
              <Route path="/hbti/detail" element={<HBTIListDetailPage />} /> 
             <Route path="/trainer/:trainerId/detail" element={<TrainerDetailPage />} />
-=======
             <Route
               path="/member/detail/write"
-              Component={TrainerDetailPage}
+              Component={TrainerDetailPageWrite}
             ></Route>
->>>>>>> TrainerDetailPage
-=======
             <Route path="/reset-password" Component={ResetPasswordPage}></Route>
             <Route
               path="/forgot-password"
               Component={ForgotPasswordPage}
             ></Route>
->>>>>>> ResetPassword
+
           </Routes>
         </LoginContextProvider>
       </BrowserRouter>
     </>
-<<<<<<< HEAD
-<<<<<<< HEAD
   )
 
 }
 
 export default App;
 
-=======
-  );
-}
-
-export default App;
->>>>>>> TrainerDetailPage
-=======
-  );
-}
-
-export default App;
->>>>>>> ResetPassword
--- fitple/src/main.jsx
+++ fitple/src/main.jsx
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-import { createRoot } from 'react-dom/client'
-
-import './index.css'
-
-
-import App from './Fitple.jsx'
-
-createRoot(document.getElementById('root')).render(
-
-    <App />
-
-
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./Fitple.jsx";
 
 createRoot(document.getElementById("root")).render(<App />);
->>>>>>> TrainerDetailPage
