bcff26dd63f99640a7a7c3634714a3d6ef1c5b91
kp-of-s <gimbhs@gmil.com>
2025-02-03 17:53:07 +0900

    feat: 집감

--- fitple/src/mypage/pages/MyPage.jsx
+++ fitple/src/mypage/pages/MyPage.jsx
@@ -1,32 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
-import { Container } from 'react-bootstrap';
-import TrainerComponent from '../components/trainer/TrainerComponent';
-import StudentComponent from '../components/student/StudentComponent';
-import NoPermissionModal from '../modal/NoPermissionModal';
-import ProfileComponent from '../components/ProfileComponent';
-import { authInfo } from '../../mainpage/apis/auth';
-import axios from 'axios';
-// import { getRole } from '../utill';
+  import React, { useEffect, useState } from 'react';
+  import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
+  import { Container } from 'react-bootstrap';
+  import TrainerComponent from '../components/trainer/TrainerComponent';
+  import StudentComponent from '../components/student/StudentComponent';
+  import NoPermissionModal from '../modal/NoPermissionModal';
+  import ProfileComponent from '../components/ProfileComponent';
+  import { authInfo } from '../../mainpage/apis/auth';
+  import axios from 'axios';
+  // import { getRole } from '../utill';
 
-const MyPage = () => {
-  // const role = authInfo();
-  const [user, setUser] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate();
+  const MyPage = () => {
+    // const role = authInfo();
+    const [user, setUser] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const accessToken = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("accessToken="))
-      ?.split("=")[1];
+    useEffect(() => {      
+      const accessToken = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("accessToken="))
+        ?.split("=")[1];
 
-    if (!accessToken) {
-      console.error("액세스 토큰이 없습니다. 로그인이 필요합니다.");
-      return;
-    }
-    
-    axios.get(`${import.meta.env.VITE_Server}/register/user`, {
+      axios.get(`${import.meta.env.VITE_Server}/register/user`, {
         withCredentials: true,
         headers: {
           Authorization: `Bearer ${accessToken}`,
@@ -34,48 +29,51 @@ const MyPage = () => {
       })
       .then((response) => {
         setUser(response.data.authority);
-        console.log("가져온 사용자 정보:", response.data);
+        // console.log("가져온 사용자 정보:", response.data);
       })
       .catch((error) => {
-        console.error("사용자 정보 가져오기 오류:", error);
-        if (error.response?.status === 401) {
-          alert("로그인이 필요합니다.");
-          window.location.href = "/login";
-        }
+        // console.error("사용자 정보 가져오기 오류:", error);
+        setUser(1);
       });
-  }, []);
 
-  const handleNoPermission = () => {
-    if (!showModal) {  setShowModal(true);  }
+      if (!accessToken) {
+        return;
+      }
+    }, []);
+
+    const handleNoPermission = () => {
+      if (!showModal) {  setShowModal(true);  }
+    };
+
+    const handleCloseModal = () => {
+      setShowModal(false);
+      navigate(-1);
+    };
+
+    //아직 불러오지 못했을 때 보여줄 부분. 차후 수정
+    if (user === null) {
+      return <div>로딩 중...</div>;
+    }
+
+    return (
+        <>
+        <ProfileComponent/>
+        <Container>
+        <NoPermissionModal show={showModal} onClose={handleCloseModal} />
+          <Routes>
+            {user === 'ROLE_TRAINER' ? (
+              <Route path='/member' element={<TrainerComponent />} />
+            ) : user === 'ROLE_STUDENT' ? (
+              <Route path='/member' element={<StudentComponent />} />
+            ) : user === 'ROLE_ADMIN' ? (
+              <Route path='/admin' element={<StudentComponent />} /> //차후 어드민 페이지 연결
+            ) : (
+              <Route path='/member' element={handleNoPermission()} />
+            )}
+        </Routes>
+        </Container>
+        </>
+    );
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
-    navigate(-1);
-  };
-
-  // 권한에 따른 렌더링??
-  if (user === null) {
-    return <div>로딩 중...</div>; // 권한 정보가 아직 로드되지 않았다면 로딩 표시
-  }
-
-  return (
-      <>
-      <ProfileComponent/>
-      <Container>
-      <NoPermissionModal show={showModal} onClose={handleCloseModal} />
-        <Routes>
-          {user === 'ROLE_TRAINER' ? (
-            <Route path='/member' element={<TrainerComponent />} />
-          ) : user === 'ROLE_STUDENT' ? (
-            <Route path='/member' element={<StudentComponent />} />
-          ) : (
-            <Route path='/member' element={handleNoPermission()} />
-          )}
-      </Routes>
-      </Container>
-      </>
-  );
-};
-
-export default MyPage;
+  export default MyPage;
