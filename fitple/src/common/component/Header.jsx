84ff501cb9559cc232573ddaec164e1db995a60d
Xuswnss <cooconut0226@gmail.com>
2025-02-14 10:34:31 +0900

    Feat(#1) : 로그아웃수정

--- fitple/src/common/component/Header.jsx
+++ fitple/src/common/component/Header.jsx
@@ -29,7 +29,11 @@ const Header = () => {
           </Link>
         )}
         {isLogin ? (
-          <Link onClick={logout(false)}>
+          <Link
+            onClick={() => {
+              logout(false);
+            }}
+          >
             <i className="bi bi-box-arrow-right logout"></i>
           </Link>
         ) : (
