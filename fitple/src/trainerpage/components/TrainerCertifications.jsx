5f4f95681a26931561fe60e3d5f29f8cfa39cb08
kp-of-s <gimbhs@gmil.com>
2025-02-17 10:55:33 +0900

    fix: 배포 후 버그 수정 3

--- fitple/src/trainerpage/components/TrainerCertifications.jsx
+++ fitple/src/trainerpage/components/TrainerCertifications.jsx
@@ -47,7 +47,7 @@ function TrainerCertifications({ certifications, BASE_URL }) {
         <div className="modal-overlay" onClick={closeModal}>
           <div className="modal-contents" onClick={(e) => e.stopPropagation()}>
             <img
-              src={`${BASE_URL}${selectedImage.replace(/^\./, "")}`}
+              src={`${BASE_URL}/${selectedImage}`}
               alt="자격증 사진"
               className="modal-images"
             />
